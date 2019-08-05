1e8ce9b41340c2cb397c5e8cb5c6ebfbc416eab8
NugiNurGhani <280896+NugiNurGhani@users.noreply.github.com>
2019-08-05 16:29:36 +0700

    latihan

--- novice/01/01/latihan/bin/main.dart
+++ novice/01/01/latihan/bin/main.dart
@@ -1,5 +1,51 @@
 import 'package:latihan/latihan.dart' as latihan;
+// Importing core libraries
+import 'dart:math';
 
-main(List<String> arguments) {
-  print('Hello world: ${latihan.calculate()}!');
+// Importing libraries from external packages
+import 'package:test/test.dart';
+
+
+void main() {
+  print('Nugi');
+
+  if (year >= 2001) {
+  print('21st century');
+} else if (year >= 1901) {
+  print('20th century');
 }
+
+for (var object in flybyObjects) {
+  print(object);
+}
+
+for (int month = 1; month <= 12; month++) {
+  print(month);
+}
+
+while (year < 2016) {
+  year += 1;
+}
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+var result = fibonacci(20);
+
+print(result);
+
+flybyObjects.where((name) => name.contains('u')).forEach(print);
+
+}
+
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
