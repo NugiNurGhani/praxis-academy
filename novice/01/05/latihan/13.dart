263937f649b3385658c05ec85af2afffcd59ecfd
NugiNurGhani <280896+NugiNurGhani@users.noreply.github.com>
2019-08-12 13:14:58 +0700

    1

--- novice/01/05/latihan/13.dart
+++ novice/01/05/latihan/13.dart
@@ -11,13 +11,17 @@ Future<void> heyHey() async {
       .then((request) => request.close()) // sends the request
       .then((response) =>
           sink.write(response.transform(Utf8Decoder()).listen(print)));
-  sink.close();
 
+
+          </>
+
+
+
+  sink.close();
   // var tes;
   // var request = await HttpClient()
   //     .getUrl(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
   // var response = await request.close();
-
   // transforms and prints the response
   // await for (var contents in response.transform(Utf8Decoder())) {
   //   print(contents);
@@ -30,7 +34,6 @@ void main() {
   // var file = new File('output.txt');
   // var sink = file.openWrite();
   // sink.write("tes");
-
   // Close the IOSink to free system resources.
   // sink.close();
 }
